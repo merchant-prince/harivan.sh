0c59a669f98d5858ecb3b6a367938596325195ce
Harivansh <hello@harivan.sh>
2022-07-03 14:40:24 +0400

    refactor: display info

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,6 @@
 <template>
   <div class="w-full h-full flex flex-col bg-slate-50">
-    <nav class="p-4 flex justify-between items-center space-x-4">
-      <a href="#">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-          />
-        </svg>
-      </a>
-      <a href="#">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7"
-          />
-        </svg>
-      </a>
-      <a href="#">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"
-          />
-        </svg>
-      </a>
-      <a href="#">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"
-          />
-        </svg>
-      </a>
-      <a href="mailto:hello@harivan.sh">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-          />
-        </svg>
-      </a>
+    <nav class="p-4 flex justify-end items-center">
       <a
         href="https://github.com/merchant-prince"
         aria-label="GitHub"
@@ -88,7 +8,7 @@
         rel="noreferrer"
       >
         <svg
-          class="h-6 w-6 text-black"
+          class="h-6 w-6 text-gray-800"
           xmlns="http://www.w3.org/2000/svg"
           role="img"
           viewBox="0 0 24 24"
@@ -106,9 +26,12 @@
       </a>
     </nav>
     <main class="p-4 flex-1 flex flex-col justify-center space-y-4">
-      <p class="font-['Josefin_Slab'] font-semibold">Hi, I'm</p>
+      <p class="font-['Josefin_Slab'] font-semibold text-slate-600 text-lg">Hi, I'm</p>
       <p class="font-['Bungee_Shade'] text-4xl text-center">Harivansh</p>
-      <p class="font-['Josefin_Slab'] font-semibold text-right">and I craft solutions using code.</p>
+      <p class="font-['Josefin_Slab'] font-semibold text-right text-slate-600 text-lg">and I craft solutions using code.</p>
     </main>
+    <footer class="p-4 flex justify-center">
+      <a href="mailto:hello@harivan.sh?subject=Hi" title="Say Hi!" class="text-center text-slate-400 font-['Coda']">hello@harivan.sh</a>
+    </footer>
   </div>
 </template>
